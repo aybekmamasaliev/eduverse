e2b97abdac67cb52d10a8d8dc66cfd8c36747113
Aibek  Mamasaaliev <aybekmamasaliev6@gmail.com>
2025-08-12 23:24:55 +0600

    feat: statistics-block is ready

--- src/app/pages/landing-content/landing-content.component.ts
+++ src/app/pages/landing-content/landing-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../components/banner/banner.component";
+import { StatisticsComponent } from "../../components/statistics/statistics.component";
 
 @Component({
   selector: 'app-landing-content',
-  imports: [BannerComponent],
+  imports: [BannerComponent, StatisticsComponent],
   templateUrl: './landing-content.component.html',
   styleUrl: './landing-content.component.scss'
 })
