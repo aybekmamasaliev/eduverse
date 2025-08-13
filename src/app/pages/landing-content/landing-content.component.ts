731d26c8dcf78e462c51820965db35d27f4a76e0
Aibek  Mamasaaliev <aybekmamasaliev6@gmail.com>
2025-08-13 18:48:41 +0600

    feat: partners-block is ready

--- src/app/pages/landing-content/landing-content.component.ts
+++ src/app/pages/landing-content/landing-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { BannerComponent } from "../../components/banner/banner.component";
 import { StatisticsComponent } from "../../components/statistics/statistics.component";
 import { FeaturesComponent } from "../../components/features/features.component";
+import { PartnersComponent } from "../../components/partners/partners.component";
 
 @Component({
   selector: 'app-landing-content',
-  imports: [BannerComponent, StatisticsComponent, FeaturesComponent],
+  imports: [BannerComponent, StatisticsComponent, FeaturesComponent, PartnersComponent],
   templateUrl: './landing-content.component.html',
   styleUrl: './landing-content.component.scss'
 })
