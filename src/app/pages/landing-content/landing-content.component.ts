07776a2b214d8a74ae0320d5915a54818dd563f1
Aibek  Mamasaaliev <aybekmamasaliev6@gmail.com>
2025-08-13 19:51:22 +0600

    feat: schools-block is ready

--- src/app/pages/landing-content/landing-content.component.ts
+++ src/app/pages/landing-content/landing-content.component.ts
@@ -3,10 +3,11 @@ import { BannerComponent } from "../../components/banner/banner.component";
 import { StatisticsComponent } from "../../components/statistics/statistics.component";
 import { FeaturesComponent } from "../../components/features/features.component";
 import { PartnersComponent } from "../../components/partners/partners.component";
+import { SchoolsComponent } from "../../components/schools/schools.component";
 
 @Component({
   selector: 'app-landing-content',
-  imports: [BannerComponent, StatisticsComponent, FeaturesComponent, PartnersComponent],
+  imports: [BannerComponent, StatisticsComponent, FeaturesComponent, PartnersComponent, SchoolsComponent],
   templateUrl: './landing-content.component.html',
   styleUrl: './landing-content.component.scss'
 })
