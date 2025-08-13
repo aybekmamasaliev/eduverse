618597e377e50f268b061fcafab096a48fa18a24
Aibek  Mamasaaliev <aybekmamasaliev6@gmail.com>
2025-08-13 13:10:41 +0600

    feat: features-block is ready

--- src/app/pages/landing-content/landing-content.component.ts
+++ src/app/pages/landing-content/landing-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../components/banner/banner.component";
 import { StatisticsComponent } from "../../components/statistics/statistics.component";
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
   selector: 'app-landing-content',
-  imports: [BannerComponent, StatisticsComponent],
+  imports: [BannerComponent, StatisticsComponent, FeaturesComponent],
   templateUrl: './landing-content.component.html',
   styleUrl: './landing-content.component.scss'
 })
